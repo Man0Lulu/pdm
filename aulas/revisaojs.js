a9d4f4b4e3a55050c99a012cf7737b1c22fc6798
Luís <luisrocksousa1111@gmail.com>
2023-08-22 20:32:06 -0300

    aula de revisão de javascript + react

---
+++ aulas/revisaojs.js
@@ -0,0 +1,47 @@
+const tarefas = [{
+    id: 01, 
+    name: "marcelinho",
+    completed: true
+},
+
+{
+    id: 02, 
+    name: " foi na escola ",
+    completed: true
+},
+
+{
+    id: 03, 
+    name: " comprar pão",
+    completed: true
+}]
+
+const pessoa = { id: 1, name: "Jose", completed: true};
+pessoa.sexo = 'M';
+pessoa = { id: 2, name: "Maria", completed: true};
+
+let count = 0;
+
+//funcoes
+const soma = (a,b) => a+b;
+
+//Em HTML se fazn <div> em react native se faz <View>
+const app = () => <View></View>;
+
+//propagacao 
+frutas.push(["uva", "maca"])
+["uva", "maca"]
+
+//desestruturacao
+const{id, nome} = pessoa;
+
+//modulos
+export default pessoa;
+
+export {frutas, soma};
+
+import pessoa from 'revisao';
+
+import {frutas} from 'revisao';
+
+import {View} from 'react-native';
