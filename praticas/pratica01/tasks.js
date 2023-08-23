680e70e56ccc2691bc6a76aa5e0ea25daceb930b
Luís <luisrocksousa1111@gmail.com>
2023-08-22 21:58:48 -0300

    codigos novos

--- praticas/pratica01/tasks.js
+++ praticas/pratica01/tasks.js
@@ -1,1 +1,22 @@
-var tarefas = []
+let tasks = [{
+    id:05,
+    name: "wallace",
+    completed: true
+    
+}]
+
+function getTasks(){
+    return tasks
+}
+
+let idTarefas = tasks[tasks .lenght - 1]
+let Tarefas = [{
+    id: idTarefas .lenght + 1,
+    name:"wallace",
+    completed: true
+}]
+
+addTasks = ({name: "batista", completed: true })
+addTasks = ({name: "andre", completed: true })
+console.log(Tarefas);
+
